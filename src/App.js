bebccfc3d9cf22d71b6d2daaf539f93f28c7aa6a
TsujimotoSirChristopherMamoru <tsujimotosirchristophermamoru@gmail.com>
2021-04-14 15:36:24 +0900

    Complete Section3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import { useState, useCallback, useMemo } from "react";
+import { ChildArea } from "../ChildArea";
 import "./styles.css";
 
 export default function App() {
+  console.log("App");
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const onChangeText = (e) => setText(e.target.value);
+
+  const onClickOpen = () => setOpen(!open);
+
+  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
+
+  const temp = useMemo(() => 1 + 3, []);
+  console.log(temp);
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <h3>Edited!</h3>
+      <input value={text} onChange={onChangeText} />
+      <br />
+      <br />
+      <button onClick={onClickOpen}>View</button>
+      <ChildArea open={open} onClickClose={onClickClose} />
     </div>
   );
 }
